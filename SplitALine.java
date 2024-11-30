eed9a40c1088046b9ddc48a92d19672b1ad3f6c5
Vikram <vikram@servx.global>
2024-11-30 16:08:33 +0530

    Modified the split program

--- SplitALine.java
+++ SplitALine.java
@@ -5,11 +5,11 @@ public class SplitALine{
         System.out.print("Enter the values : ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        String str = sc.next().replace(" ", "");
-        char ch = sc.next().replace(" ", "").charAt(0);
+        String str = sc.next();
+        char ch = sc.next().charAt(0);
         System.out.println(num);
         System.out.println(str);
         System.out.println(ch);
-        sc.close();;
+        sc.close();
     }
 }
