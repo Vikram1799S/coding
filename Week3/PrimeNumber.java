c9a5c7b28d4bf9f8cd093f96246cd56c8625932c
Vikram1799S <vikram1799s@gmail.com>
2024-12-12 18:23:13 +0530

    Create PrimeNumber.java

---
+++ Week3/PrimeNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        if(n>1){
+            for(int i = 2; i<=n ; i++){
+                boolean flag = true;
+                for(int j=2; j<i;j++){
+                    if(i%j == 0)
+                        flag = false; 
+                }
+                if(flag) System.out.println(i);
+            }
+        }else if(n==1)
+            System.out.println(n);
+        else System.out.print("Enter the valid number!");
+        sc.close();
+    }
+}
